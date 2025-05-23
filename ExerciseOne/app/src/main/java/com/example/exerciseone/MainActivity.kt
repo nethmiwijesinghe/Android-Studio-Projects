1aa0648b9da621fca14a8e165cccd758b5905bdd
Nethmi Wijesinghe <nethmiwijesinghe69@gmail.com>
2025-05-23 14:46:59 +0530

    Update MainActivity and activity_main layout for hobby selection functionality

--- ExerciseOne/app/src/main/java/com/example/exerciseone/MainActivity.kt
+++ ExerciseOne/app/src/main/java/com/example/exerciseone/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.exerciseone
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val checkboxReading = findViewById<CheckBox>(R.id.checkBoxReading)
+        val checkboxTravelling = findViewById<CheckBox>(R.id.checkBoxTravelling)
+        val checkboxCooking = findViewById<CheckBox>(R.id.checkBoxCooking)
+        val checkboxGaming = findViewById<CheckBox>(R.id.checkBoxGaming)
+        val btnShowSelection = findViewById<Button>(R.id.btnShowSelection)
+        val textResult = findViewById<TextView>(R.id.textResult)
+
+        btnShowSelection.setOnClickListener{
+            val selectedHobbies = StringBuilder("Selected hobbies:\n")
+
+            if (checkboxReading.isChecked) selectedHobbies.append("- Reading\n")
+            if (checkboxTravelling.isChecked) selectedHobbies.append("- Traveling\n")
+            if (checkboxGaming.isChecked) selectedHobbies.append("- Gaming\n")
+            if (checkboxCooking.isChecked) selectedHobbies.append("- Cooking\n")
+
+            if (selectedHobbies.toString() == "Selected Hobbies: \n") {
+                textResult.text = "No hobbies selected"
+            } else {
+                textResult.text = selectedHobbies.toString()
+            }
         }
     }
 }
