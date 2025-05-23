5af4149864b4f5441a4fcf660bc7f251f142194d
Nethmi Wijesinghe <nethmiwijesinghe69@gmail.com>
2025-05-23 11:50:47 +0530

    Add ThirdActivity and update SecondActivity with navigation button

--- ExpilicitlntentDemo/app/src/main/java/com/example/expilicitlntentdemo/SecondActivity.kt
+++ ExpilicitlntentDemo/app/src/main/java/com/example/expilicitlntentdemo/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.expilicitlntentdemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,9 +13,15 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val button = findViewById<Button>(R.id.buttonThanks)
 
         val textview = findViewById<TextView>(R.id.textviewResult)
         val name = intent.getStringExtra("user_name")
         textview.text = "Welcome, $name!"
+
+        button.setOnClickListener{
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ ExpilicitlntentDemo/app/src/main/java/com/example/expilicitlntentdemo/ThirdActivity.kt
@@ -0,0 +1,18 @@
+package com.example.expilicitlntentdemo
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+
+        val textview = findViewById<TextView>(R.id.textViewThankYou)
+        textview.text = "Thank You!"
+    }
+}
